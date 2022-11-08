9ac3225f9b159d9bf47a8a723a0212aea1dd8e7c
Aziz <aziznurulloh8@gmail.com>
2022-11-08 11:44:30 +0700

    CHG helper

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,7 +1,7 @@
 const {Pool} = require("pg");
 
 const db = new Pool({
-  connectionString: "postgresql://postgres:1@localhost:5432/postgres?schema=public"
+  connectionString: "postgresql://postgres:Posrgres!123@db.wcetkwnsasmhnzwhjixf.supabase.co:5432/postgres?schema=public"
 });
 
 module.exports = db;
